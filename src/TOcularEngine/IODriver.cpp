7d652180928c8637e6d18ab2d89fcf48717a85d3
angelnm <angel.nm@hotmail.es>
2018-02-25 16:12:40 +0100

    Input

--- src/TOcularEngine/IODriver.cpp
+++ src/TOcularEngine/IODriver.cpp
@@ -1,4 +1,9 @@
 #include "IODriver.h"
+#include <iostream>
+
+TEvent::TEvent(){}
+
+TEvent::~TEvent(){}
 
 IODriver::IODriver(){
     m_close = false;
@@ -7,29 +12,202 @@ IODriver::IODriver(){
 IODriver::~IODriver(){}
 
 bool IODriver::Update(sf::Event* event){
-    if (event->type == sf::Event::Closed) m_close = true;
 
-    if(event->type == sf::Event::KeyPressed){
-        if (event->key.code == sf::Keyboard::Escape){
-        m_close = true;
+    TEvent sfEvent;
+
+    sfEvent.m_type = Type_Unknow;
+    switch(event->type){
+        case sf::Event::Closed:{
+            sfEvent.m_type = Type_Closed;
+            // No Data
+            break;
         }
-        if (event->key.code == sf::Keyboard::Left){
-            //addVertices(-2, 0);
+        case sf::Event::Resized:{
+            sfEvent.m_type = Type_Resized;
+            // Window Size
+            sfEvent.m_size.width     = event->size.width;
+            sfEvent.m_size.height    = event->size.height;
+            break;
         }
-        if (event->key.code == sf::Keyboard::Right){
-            //addVertices(2, 0);
+        case sf::Event::LostFocus:{
+            sfEvent.m_type = Type_LostFocus;
+            // No Data
+            break;
         }
-        if (event->key.code == sf::Keyboard::Down){
-            //addVertices(0, -2);
+        case sf::Event::GainedFocus:{
+            sfEvent.m_type = Type_GainedFocus;
+            // No Data
+            break;
         }
-        if (event->key.code == sf::Keyboard::Up){
-            //addVertices(0, 2);				
+        case sf::Event::TextEntered:{
+            sfEvent.m_type = Type_TextEntered;
+            // Char Enter
+            sfEvent.m_text.unicode = event->text.unicode; 
+            break;
+        }
+        case sf::Event::KeyPressed:{
+            sfEvent.m_type = Type_KeyPressed;
+            // Key Data
+            sfEvent.m_key.code     = (KeyboardKey)event->key.code;
+            sfEvent.m_key.alt      = event->key.alt;
+            sfEvent.m_key.control  = event->key.control;
+            sfEvent.m_key.shift    = event->key.shift;
+            sfEvent.m_key.system   = event->key.system;
+            break;
+        }
+        case sf::Event::KeyReleased:{
+            sfEvent.m_type = Type_KeyReleased;
+            // Key Data
+            sfEvent.m_key.code     = (KeyboardKey)event->key.code;
+            sfEvent.m_key.alt      = event->key.alt;
+            sfEvent.m_key.control  = event->key.control;
+            sfEvent.m_key.shift    = event->key.shift;
+            sfEvent.m_key.system   = event->key.system;
+            break;
+        }
+        case sf::Event::MouseWheelScrolled:{
+            sfEvent.m_type = Type_MouseWheelScrolled;
+            // Mouse Wheel
+            sfEvent.m_mouseWheel.delta = event->mouseWheel.delta;
+            sfEvent.m_mouseWheel.x     = event->mouseWheel.x;
+            sfEvent.m_mouseWheel.y     = event->mouseWheel.y;
+            break;
+        }
+        case sf::Event::MouseButtonPressed:{
+            sfEvent.m_type = Type_MouseButtonPressed;
+            // Mouse Button
+            sfEvent.m_mouseButton.button   = (ButtonMouse)event->mouseButton.button;
+            sfEvent.m_mouseButton.x        = event->mouseButton.x;
+            sfEvent.m_mouseButton.y        = event->mouseButton.y;
+            break;
+        }
+        case sf::Event::MouseButtonReleased:{
+            sfEvent.m_type = Type_MouseButtonReleased;
+            // Mouse Button
+            sfEvent.m_mouseButton.button   = (ButtonMouse)event->mouseButton.button;
+            sfEvent.m_mouseButton.x        = event->mouseButton.x;
+            sfEvent.m_mouseButton.y        = event->mouseButton.y;
+            break;
+        }
+        case sf::Event::MouseMoved:{
+            sfEvent.m_type = Type_MouseMoved;
+            // Mouse Move
+            sfEvent.m_mouseMove.x = event->mouseMove.x;
+            sfEvent.m_mouseMove.y = event->mouseMove.y;
+            break;
+        }
+        case sf::Event::MouseEntered:{
+            sfEvent.m_type = Type_MouseEntered;
+            // No Data
+            break;
+        }
+        case sf::Event::MouseLeft:{
+            sfEvent.m_type = Type_MouseLeft;
+            // No Data
+            break;
+        }
+        case sf::Event::JoystickButtonPressed:{
+            sfEvent.m_type = Type_JoystickButtonPressed;
+            // Joystick button
+            sfEvent.m_joystickButton.joystickId    = event->joystickButton.joystickId;
+            sfEvent.m_joystickButton.button        = event->joystickButton.button;
+            break;
+        }
+        case sf::Event::JoystickButtonReleased:{
+            sfEvent.m_type = Type_JoystickButtonReleased;
+            // Joystick button
+            sfEvent.m_joystickButton.joystickId    = event->joystickButton.joystickId;
+            sfEvent.m_joystickButton.button        = event->joystickButton.button;
+            break;
+        }
+        case sf::Event::JoystickMoved:{
+            sfEvent.m_type = Type_JoystickMoved;
+            // Joystick move
+            sfEvent.m_joystickMove.joystickId  = event->joystickMove.joystickId;
+            sfEvent.m_joystickMove.axis        = (JoystickAxis)event->joystickMove.axis;
+            sfEvent.m_joystickMove.position    = event->joystickMove.position;
+            break;
+        }
+        case sf::Event::JoystickConnected:{
+            sfEvent.m_type = Type_JoystickConnected;
+            // Joystick id
+            sfEvent.m_joystickConnect.joystickId = event->joystickConnect.joystickId;
+            break;
+        }
+        case sf::Event::JoystickDisconnected:{
+            sfEvent.m_type = Type_JoystickDisconnected;
+            // Joystick id
+            sfEvent.m_joystickConnect.joystickId = event->joystickConnect.joystickId;
+            break;
+        }
+        case sf::Event::TouchBegan:{
+            sfEvent.m_type = Type_TouchBegan;
+            // Touch 
+            sfEvent.m_touch.finger     = event->touch.finger;
+            sfEvent.m_touch.x          = event->touch.x;
+            sfEvent.m_touch.y          = event->touch.y;
+            break;
+        }
+        case sf::Event::TouchMoved:{
+            sfEvent.m_type = Type_TouchMoved;
+            // Touch 
+            sfEvent.m_touch.finger     = event->touch.finger;
+            sfEvent.m_touch.x          = event->touch.x;
+            sfEvent.m_touch.y          = event->touch.y;
+            break;
+        }
+        case sf::Event::TouchEnded:{
+            sfEvent.m_type = Type_TouchEnded;
+            // Touch 
+            sfEvent.m_touch.finger     = event->touch.finger;
+            sfEvent.m_touch.x          = event->touch.x;
+            sfEvent.m_touch.y          = event->touch.y;
+            break;
+        }
+        case sf::Event::SensorChanged:{
+            sfEvent.m_type = Type_SensorChanged;
+            // Sensor
+            sfEvent.m_sensor.type  = (SensorType)event->sensor.type;
+            sfEvent.m_sensor.x     = event->sensor.x;
+            sfEvent.m_sensor.y     = event->sensor.y;
+            sfEvent.m_sensor.z     = event->sensor.z;
+            break;
+        }
+        default:{
+            break;
         }
     }
 
-    if (event->type == sf::Event::KeyReleased){ 
-        if(event->key.code == sf::Keyboard::Space){
+    const TEvent* pointer = &sfEvent;
 
+    return OnEvent(*pointer);
+}
+
+
+bool IODriver::OnEvent(const TEvent& event){
+    if (event.m_type == Type_Closed) m_close = true;
+
+    if(event.m_type == Type_KeyPressed){
+        if (event.m_key.code == Key_Escape){
+            m_close = true;
+        }
+        if (event.m_key.code == Key_Left){
+            //addVertices(-2, 0);
+        }
+        if (event.m_key.code == Key_Right){
+            //addVertices(2, 0);
+        }
+        if (event.m_key.code == Key_Down){
+            //addVertices(0, -2);
+        }
+        if (event.m_key.code == Key_Up){
+            //addVertices(0, 2);                
+        }
+    }
+
+    if (event.m_type == Type_KeyReleased){ 
+        if(event.m_key.code == Key_Space){
+            std::cout<<"funciona"<<std::endl;
         } 
     }
     return m_close;
--- src/TOcularEngine/IODriver.h
+++ src/TOcularEngine/IODriver.h
@@ -1,14 +1,103 @@
 #ifndef IODRIVER_H
 #define IODRIVER_H
 #include <SFML/Graphics.hpp>
+#include <EventEnum.h>
+
+class TEvent{
+public:
+	TEvent();
+	~TEvent();
+
+
+	struct SizeEvent{
+        unsigned int 	width;  
+        unsigned int 	height; 
+    };
+
+    struct TextEvent{
+        unsigned int 	unicode; 
+    };
+
+    struct KeyEvent{
+        KeyboardKey 	code;    
+        bool         	alt;     
+        bool          	control; 
+        bool          	shift;   
+        bool          	system;  
+    };
+
+    struct MouseWheelEvent{
+        int 			delta; 
+        int 			x;     
+        int 			y;     
+    };
+
+    struct MouseButtonEvent{
+        ButtonMouse 	button; 
+        int           	x;      
+        int           	y;      
+    };
+
+    struct MouseMoveEvent{
+        int x; 
+        int y; 
+    };
+
+    struct JoystickButtonEvent{
+        unsigned int joystickId; 
+        unsigned int button;     
+    };
+
+    struct JoystickMoveEvent{
+        unsigned int   		joystickId; 
+        JoystickAxis	 	axis;       
+        float          		position;   
+    };
+
+    struct JoystickConnectEvent{
+        unsigned int joystickId; 
+    };
+
+    struct TouchEvent{
+        unsigned int finger; 
+        int x;               
+        int y;               
+    };
+
+    struct SensorEvent{
+        SensorType 	type; 
+        float 		x;           
+        float 		y;           
+        float 		z;           
+    };
+
+	// Union: Todos los datos comparten el mismo espacio, haciendo que ahorre espacio
+	// Como esta pensado para que solo gaste un tipo de dato no deberia haber problema
+	union{
+		SizeEvent				m_size;
+		TextEvent				m_text;
+		KeyEvent				m_key;
+		MouseWheelEvent 		m_mouseWheel;
+		MouseButtonEvent 		m_mouseButton;
+		MouseMoveEvent			m_mouseMove;
+		JoystickButtonEvent		m_joystickButton;
+		JoystickMoveEvent		m_joystickMove;
+		JoystickConnectEvent 	m_joystickConnect; 
+		TouchEvent				m_touch;
+		SensorEvent				m_sensor;
+	};
+
+	EventType m_type;
+	
+};
 
 class IODriver{
 public:
     IODriver();
     ~IODriver();
     bool Update(sf::Event* event);
-
 private:
+	bool OnEvent(const TEvent& event);
     bool m_close;
 };
 
