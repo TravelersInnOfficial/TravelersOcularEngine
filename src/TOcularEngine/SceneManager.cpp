5fbe1c5a43a3263a14a57f7f297ec31ec238c30c
DanielFinestrat <danielfinestratmartinez@hotmail.com>
2018-03-11 13:20:03 +0100

    Better moving skydome

--- src/TOcularEngine/SceneManager.cpp
+++ src/TOcularEngine/SceneManager.cpp
@@ -87,7 +87,7 @@ TFMesh* SceneManager::AddMesh(toe::core::TOEvector3df position, toe::core::TOEve
 
 TFDome* SceneManager::AddDome(toe::core::TOEvector3df position, std::string texturePath){
 	if(m_dome == nullptr){
-		TFDome* toRet = new TFDome(position, toe::core::TOEvector3df(0.0f,0.0f,0.0f), toe::core::TOEvector3df(5.0f,5.0f,5.0f), texturePath);
+		TFDome* toRet = new TFDome(position, toe::core::TOEvector3df(0.0f,0.0f,0.0f), toe::core::TOEvector3df(50.0f,50.0f,50.0f), texturePath);
 		m_dome = toRet;
 		m_dome->AttachFirst(m_SceneTreeRoot);	
 	}
