3068ba930bd9faf67536cb5d16fd6b7f8311f041
angelnm <angel.nm@hotmail.es>
2018-03-13 12:45:39 +0100

    Camara

--- src/EventHandler.cpp
+++ src/EventHandler.cpp
@@ -1,5 +1,6 @@
 #include "EventHandler.h"
 #include "TOcularEngine/VideoDriver.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 float EventHandler::xdist = 0.0f;
 float EventHandler::ydist = 3.0f;
@@ -20,12 +21,19 @@ bool EventHandler::OnEvent(const TEvent& event){
     if(event.m_type == Type_KeyPressed){
         if (event.m_key.code == Key_Escape) m_close = true;
 
-        if (event.m_key.code == Key_A) xdist -= 1.0f;
-        if (event.m_key.code == Key_D) xdist += 1.0f;
-        if (event.m_key.code == Key_S) zdist -= 1.0f;
-        if (event.m_key.code == Key_W) zdist += 1.0f;
-        if (event.m_key.code == Key_Up) ydist += 1.0f;
-        if (event.m_key.code == Key_Down) ydist -= 1.0f;
+        if (event.m_key.code == Key_A || event.m_key.code == Key_D || event.m_key.code == Key_S || event.m_key.code == Key_W){
+        float max = 1;
+        float giroY = ydistGiro;
+        float giroX = xdistGiro;
+        if (event.m_key.code == Key_A){ giroY -= 90.0f; giroX = 0.0f;}
+        if (event.m_key.code == Key_D){ giroY += 90.0f; giroX = 0.0f;}
+        if (event.m_key.code == Key_S) max = -1.0f;
+        if (event.m_key.code == Key_W) max = 1.0f;
+
+        xdist += sin(glm::radians(giroY))*cos(glm::radians(giroX))*max;
+        ydist -= sin(glm::radians(giroX))*max;
+        zdist += cos(glm::radians(giroY))*cos(glm::radians(giroX))*max;
+        }
     }
 
     if(event.m_type == Type_MouseMoved){
