33a8a50667b61c077a1c6fd523e5d9596e483541
angelnm <angel.nm@hotmail.es>
2018-05-23 20:49:54 +0200

    TOcularEngine comentado

--- src/TOcularEngine/TOcularEngine.h
+++ src/TOcularEngine/TOcularEngine.h
@@ -4,36 +4,124 @@
 #include "VideoDriver.h"
 
 namespace toe{
-    //PUBLIC
+    /**
+     * @brief   - Devuelve el VideoDriver que es singleton
+     */
     VideoDriver* GetVideoDriver();
 
+    /**
+     * @brief   - Devuelve el SceneManager que es singleton 
+     */
     SceneManager* GetSceneManager();
 
+    /**
+     * @brief   - Anyade un cube a la escena 
+     * 
+     * @param   - position - Posicion del cubo
+     * @param   - rotation - Rotacion del cubo
+     * @param   - scale - Tamanyo del cubo
+     * @return  - TFMesh - Cubo creado
+     */
     TFMesh* AddCube(TOEvector3df position = TOEvector3df(0,0,0),
                  TOEvector3df rotation = TOEvector3df(0,0,0),
                  TOEvector3df scale = TOEvector3df(1,1,1));
 
+    /**
+     * @brief   - Anyade una esfera a la escena 
+     * 
+     * @param   - position - Posicion de la esfera
+     * @param   - rotation - Rotacion de la esfera
+     * @param   - scale - Tamanyo de la esfera
+     * @return  - TFMesh - Esfera creada
+     */
     TFMesh* AddSphere(TOEvector3df position = TOEvector3df(0,0,0),
                  TOEvector3df rotation = TOEvector3df(0,0,0),
                  TOEvector3df scale = TOEvector3df(1,1,1));
 
+    /**
+     * @brief   - Anyade un rectangulo 2D a la escena 
+     * 
+     * @param   - position - Posicion del rectangulo 
+     * @param   - size - Tamanyo del rectangulo
+     * @param   - rotation - Rotacion del rectangulo
+     * @return  - TFRect - Rectangulo creado
+     */
     TFRect* Add2DRect(TOEvector2df position = TOEvector2df(0.0f),
                  TOEvector2df size = TOEvector2df(10,10),
                  float rotation = 0.0f);
 
+    /**
+     * @brief   - Anyade un texto 2D a la escena
+     *  
+     * @param   - text - Cadena a mostrar
+     * @param   - position - Posicion del texto
+     * @return  - TF2DTexto - Texto 2D creado
+     */
     TF2DText* Add2DText(std::string text = "",
                  TOEvector2df position = TOEvector2df(0.0f,0.0f));
 
+    /**
+     * @brief   - Anyade un Sprite a la escena 
+     * 
+     * @param   - texture - Ruta a la textura a mostrar
+     * @param   - position - Posicion del SPrite
+     * @param   - size - Tamanyo del Sprite
+     * @return  - TFSprite - Sprite creado
+     */
     TFSprite* AddSprite(std::string texture = "", TOEvector2df position = TOEvector2df(0,0), TOEvector2df size = TOEvector2df(10,10));
 
+    /**
+     * @brief   - Carga un mesh 
+     * 
+     * @param   - path - Ruta del mesh 
+     */
     void LoadMesh(std::string path);
+
+    /**
+     * @brief   - Carga una textura 
+     * 
+     * @param   - path - Ruta de la textura 
+     */
     void LoadTexture(std::string path);
+
+    /**
+     * @brief   - Elimina una textura 
+     * 
+     * @param   - path - Ruta de la textura a eliminar 
+     */
     void UnloadTexture(std::string path);
+
+    /**
+     * @brief   - Consigue el puntero de Opengl de una textura 
+     * 
+     * @param   - path - Ruta de la textura 
+     */
     GLuint GetTextureID(std::string path);
+
+    /**
+     * @brief   - Consigue las dimensiones de una textura 
+     * 
+     * @param   - path - Ruta de la textura a la que conseguir la textura 
+     */
     TOEvector2di GetTextureDims(std::string path);
+
+    /**
+     * @brief   - Consigue el ancho de una textura 
+     * 
+     * @param   - path - Ruta de la textura de la que conseguir el ancho 
+     */
     int GetTextureWidth(std::string path);
+
+    /**
+     * @brief   - Consigue el alto de una textura 
+     * 
+     * @param   - path - Ruta de la textura de la que conseguir el alto 
+     */
     int GetTextureHeight(std::string path);
 
+    /**
+     * @brief   - Clase creada para poder almacenar la posicion y tamanyo de un rectangulo
+     */
     namespace core{ 
         class rect{
             public:
@@ -56,11 +144,6 @@ namespace toe{
         SceneManager* privateSceneManager;
         IODriver* privateIOManager;
     }
-
-    // TODO:: GUARDARSE TODOS LOS PUNTEROS A NODOS QUE CONTIENEN LUCES Y CAMARAS
-    //- Metodos para el registro y manejo de viewports
-    //- Metodos para el registro y manejo de camaras
-    //- Metodos para el registro y manejo de luces
 };
 
 #endif
